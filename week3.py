65ef46a6433eb777a5ce57935787b8fbdcb5f1d5
mohammedalusi <mohammedalusi9@gmail.com>
2026-03-30 14:41:17 +0300

    update

--- week3.py
+++ week3.py
@@ -1,27 +1,54 @@
-from flask import Flask
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if valid_login(request.form['username'],
-                       request.form['password']):
-            return log_the_user_in(request.form['username'])
-        else:
-            error = 'Invalid username/password'
-    # the code below is executed if the request method
-    # was GET or the credentials were invalid
-    return render_template('login.html', error=error) 
+# In-memory "database"
+todos = []
 
-@app.get('/login')
-def login_get():
-    return show_the_login_form()
+# Serve HTML page
+@app.route("/")
+def home():
+    return render_template("week3.html")
 
-@app.post('/login')
-def login_post():
-    return do_the_login()
+# GET all todos
+@app.route("/todos", methods=["GET"])
+def get_todos():
+    return jsonify(todos)
+
+# POST a new todo
+@app.route("/todos", methods=["POST"])
+def add_todo():
+    data = request.get_json()
+    task = data.get("task")
+    if not task:
+        return jsonify({"error": "Task cannot be empty"}), 400
+
+    todo = {
+        "id": len(todos) + 1,
+        "task": task,
+        "done": False
+    }
+    todos.append(todo)
+    return jsonify(todo), 201
+
+# PUT – mark todo as done
+@app.route("/todos/<int:id>", methods=["PUT"])
+def update_todo(id):
+    data = request.get_json()
+    for todo in todos:
+        if todo["id"] == id:
+            todo["done"] = data.get("done", todo["done"])
+            return jsonify(todo)
+    return jsonify({"error": "Not found"}), 404
+
+# DELETE a todo
+@app.route("/todos/<int:id>", methods=["DELETE"])
+def delete_todo(id):
+    for todo in todos:
+        if todo["id"] == id:
+            todos.remove(todo)
+            return jsonify({"message": "Deleted"})
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
